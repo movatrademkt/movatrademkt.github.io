a338097e5b2e5f99120a4efcaea1b726985cd060
movatrademkt <46026426+movatrademkt@users.noreply.github.com>
2023-11-06 09:23:51 -0300

    4

--- resid/data/slide1.js
+++ resid/data/slide1.js
@@ -1,9 +1,9 @@
-(function(){var loadHandler=window['sl_{DB513873-368C-43CB-8819-D5CFD5624A59}'];loadHandler&&loadHandler(0, '<div id="spr0_38ba0e3d"><div id="spr1_38ba0e3d" class="kern slide"><img id="img5_38ba0e3d" src="data/img5.jpg" width="960px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_38ba0e3d" class="kern slide"><div id="spr3_38ba0e3d"><img id="img0_38ba0e3d" src="data/img0.png" width="960" height="540" alt="Padrão do plano de fundo\
+(function(){var loadHandler=window['sl_{FD0F232A-347B-4170-A36F-175D829D45CF}'];loadHandler&&loadHandler(0, '<div id="spr0_6afadf6d"><div id="spr1_6afadf6d" class="kern slide"><img id="img5_6afadf6d" src="data/img5.jpg" width="960px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_6afadf6d" class="kern slide"><div id="spr3_6afadf6d"><img id="img0_6afadf6d" src="data/img0.png" width="960" height="540" alt="Padrão do plano de fundo\
 \
-Descrição gerada automaticamente"/></div><div id="spr4_38ba0e3d" style="left:77.592px;top:219.289px;"><img id="img1_38ba0e3d" src="data/img1.png" width="597" height="128" alt="Uma imagem contendo Ícone\
+Descrição gerada automaticamente"/></div><div id="spr4_6afadf6d" style="left:77.592px;top:219.289px;"><img id="img1_6afadf6d" src="data/img1.png" width="597" height="128" alt="Uma imagem contendo Ícone\
 \
-Descrição gerada automaticamente" style="left:-0.025px;top:-0.221px;"/></div><div id="spr5_38ba0e3d" style="left:77.592px;top:358.158px;"><img id="img2_38ba0e3d" src="data/img2.png" width="47" height="47" alt="Logotipo\
+Descrição gerada automaticamente" style="left:-0.025px;top:-0.221px;"/></div><div id="spr5_6afadf6d" style="left:77.592px;top:358.158px;"><img id="img2_6afadf6d" src="data/img2.png" width="47" height="47" alt="Logotipo\
 \
-Descrição gerada automaticamente" style="left:-0.062px;top:-0.062px;"/></div><div id="spr6_38ba0e3d" style="left:143.753px;top:352.134px;"><img id="img3_38ba0e3d" src="data/img3.png" width="165" height="50" alt="Logotipo\
+Descrição gerada automaticamente" style="left:-0.062px;top:-0.062px;"/></div><div id="spr6_6afadf6d" style="left:143.753px;top:352.134px;"><img id="img3_6afadf6d" src="data/img3.png" width="165" height="50" alt="Logotipo\
 \
-Descrição gerada automaticamente com confiança média" style="left:-0.238px;top:0.021px;"/></div><div id="spr7_38ba0e3d" style="left:74.156px;top:94.646px;"><img id="img4_38ba0e3d" src="data/img4.png" width="124" height="98" alt="Cena suburbana com preenchimento sólido" style="left:3.922px;top:14.922px;"/></div></div></div>');})();
+Descrição gerada automaticamente com confiança média" style="left:-0.238px;top:0.021px;"/></div><div id="spr7_6afadf6d" style="left:74.156px;top:94.646px;"><img id="img4_6afadf6d" src="data/img4.png" width="124" height="98" alt="Cena suburbana com preenchimento sólido" style="left:3.922px;top:14.922px;"/></div></div></div>');})();
