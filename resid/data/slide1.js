e06268d2d9b710eaf3421f2a915a6fa8e925cd14
movatrademkt <46026426+movatrademkt@users.noreply.github.com>
2024-03-06 14:23:45 -0300

    v1

--- resid/data/slide1.js
+++
@@ -1,9 +0,0 @@
-(function(){var loadHandler=window['sl_{1A2AEB16-1128-4F48-B4A5-1C0AECBC28C9}'];loadHandler&&loadHandler(0, '<div id="spr0_c207bdd"><div id="spr1_c207bdd" class="kern slide"><img id="img5_c207bdd" src="data/img5.jpg" width="960px" height="540px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_c207bdd" class="kern slide"><div id="spr3_c207bdd"><img id="img0_c207bdd" src="data/img0.png" width="960" height="540" alt="Padrão do plano de fundo\
-\
-Descrição gerada automaticamente"/></div><div id="spr4_c207bdd" style="left:77.592px;top:219.289px;"><img id="img1_c207bdd" src="data/img1.png" width="597" height="128" alt="Uma imagem contendo Ícone\
-\
-Descrição gerada automaticamente" style="left:-0.025px;top:-0.221px;"/></div><div id="spr5_c207bdd" style="left:77.592px;top:358.158px;"><img id="img2_c207bdd" src="data/img2.png" width="47" height="47" alt="Logotipo\
-\
-Descrição gerada automaticamente" style="left:-0.062px;top:-0.062px;"/></div><div id="spr6_c207bdd" style="left:143.753px;top:352.134px;"><img id="img3_c207bdd" src="data/img3.png" width="165" height="50" alt="Logotipo\
-\
-Descrição gerada automaticamente com confiança média" style="left:-0.238px;top:0.021px;"/></div><div id="spr7_c207bdd" style="left:74.156px;top:94.646px;"><img id="img4_c207bdd" src="data/img4.png" width="124" height="98" alt="Cena suburbana com preenchimento sólido" style="left:3.922px;top:14.922px;"/></div></div></div>');})();
