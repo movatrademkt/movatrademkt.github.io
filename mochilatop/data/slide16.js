2286840930b00bc53e3c0fb195814a4417aefbc6
movatrademkt <46026426+movatrademkt@users.noreply.github.com>
2024-03-15 17:24:22 -0300

    v3

---
+++ mochilatop/data/slide16.js
@@ -0,0 +1,35 @@
+(function(){var loadHandler=window['sl_{85497191-A758-4D7B-9918-A383D5687DFC}'];loadHandler&&loadHandler(15, '<div id="spr0_1380021"><div id="spr1_1380021" class="kern slide"><img id="img17_1380021" src="data/img10.jpg" width="720px" height="405px" alt="" style="left:0px;top:0px;"/><div id="spr3_1380021" style="left:344.06px;top:393px;"><div style="width:0px;"><span id="txt0_1380021" class="nokern" data-width="31.923828" style="top:-1.265px;">Internal</span></div></div></div><div id="spr2_1380021" class="kern slide"><div id="spr4_1380021" style="left:1.74px;top:1.26px;"><img id="img0_1380021" src="data/img0.jpg" width="720" height="405" alt="Padrão do plano de fundo\
+\
+Descrição gerada automaticamente"/></div><div id="spr5_1380021" style="left:569.768px;top:-710.602px;"><img id="img1_1380021" src="data/img36.png" width="70" height="70" alt="Ícone\
+\
+Descrição gerada automaticamente" style="left:0.162px;top:-0.198px;"/></div><div id="spr6_1380021" style="left:429.293px;top:-777.325px;"><img id="img2_1380021" src="data/img36.png" width="70" height="70" alt="Ícone\
+\
+Descrição gerada automaticamente" style="left:0.162px;top:-0.198px;"/></div><div id="spr7_1380021" style="left:489.682px;top:-346.628px;"><img id="img3_1380021" src="data/img36.png" width="70" height="70" alt="Ícone\
+\
+Descrição gerada automaticamente" style="left:0.162px;top:-0.198px;"/></div><div id="spr8_1380021" style="left:89.966px;top:-186.457px;"><img id="img4_1380021" src="data/img36.png" width="70" height="70" alt="Ícone\
+\
+Descrição gerada automaticamente" style="left:0.162px;top:-0.198px;"/></div><div id="spr9_1380021" style="left:506.516px;top:-105.733px;"><img id="img5_1380021" src="data/img36.png" width="70" height="70" alt="Ícone\
+\
+Descrição gerada automaticamente" style="left:0.162px;top:-0.198px;"/></div><div id="spr10_1380021" style="left:181.708px;top:-333.553px;"><img id="img6_1380021" src="data/img36.png" width="70" height="70" alt="Ícone\
+\
+Descrição gerada automaticamente" style="left:0.162px;top:-0.198px;"/></div><div id="spr11_1380021" style="left:43.529px;top:-434.921px;"><img id="img7_1380021" src="data/img36.png" width="70" height="70" alt="Ícone\
+\
+Descrição gerada automaticamente" style="left:0.162px;top:-0.198px;"/></div><div id="spr12_1380021" style="left:412.08px;top:-302.725px;"><img id="img8_1380021" src="data/img36.png" width="70" height="70" alt="Ícone\
+\
+Descrição gerada automaticamente" style="left:0.162px;top:-0.198px;"/></div><div id="spr13_1380021" style="left:283.984px;top:-226.873px;"><img id="img9_1380021" src="data/img36.png" width="70" height="70" alt="Ícone\
+\
+Descrição gerada automaticamente" style="left:0.162px;top:-0.198px;"/></div><div id="spr14_1380021" style="left:282.334px;top:-501.644px;"><img id="img10_1380021" src="data/img36.png" width="70" height="70" alt="Ícone\
+\
+Descrição gerada automaticamente" style="left:0.162px;top:-0.198px;"/></div><div id="spr15_1380021" style="left:125.129px;top:-554.677px;"><img id="img11_1380021" src="data/img36.png" width="70" height="70" alt="Ícone\
+\
+Descrição gerada automaticamente" style="left:0.162px;top:-0.198px;"/></div><div id="spr16_1380021" style="left:471.354px;top:-554.854px;"><img id="img12_1380021" src="data/img36.png" width="70" height="70" alt="Ícone\
+\
+Descrição gerada automaticamente" style="left:0.162px;top:-0.198px;"/></div><div id="spr17_1380021" style="left:651.415px;top:-416.232px;"><img id="img13_1380021" src="data/img36.png" width="70" height="70" alt="Ícone\
+\
+Descrição gerada automaticamente" style="left:0.162px;top:-0.198px;"/></div><div id="spr18_1380021" style="left:341.755px;top:-830.357px;"><img id="img14_1380021" src="data/img36.png" width="70" height="70" alt="Ícone\
+\
+Descrição gerada automaticamente" style="left:0.162px;top:-0.198px;"/></div><div id="spr19_1380021" style="left:54.804px;top:-830.535px;"><img id="img15_1380021" src="data/img36.png" width="70" height="70" alt="Ícone\
+\
+Descrição gerada automaticamente" style="left:0.162px;top:-0.198px;"/></div><div id="spr20_1380021" style="left:292.861px;top:-691.913px;"><img id="img16_1380021" src="data/img36.png" width="70" height="70" alt="Ícone\
+\
+Descrição gerada automaticamente" style="left:0.162px;top:-0.198px;"/></div><div id="spr21_1380021" style="left:48.88px;top:41.298px;"><div style="width:0px;"><span id="txt1_1380021" class="nokern" data-width="130.957031" style="top:-10.182px;">SORTEIO</span></div></div><div id="spr22_1380021" style="left:516.154px;top:213.084px;"><div style="width:0px;"><span id="txt2_1380021" class="nokern" data-width="82.066406" style="left:26.082px;top:-0.082px;">Sorteio é líquido</span></div><div style="width:0px;"><span id="txt3_1380021" class="nokern" data-width="107.525391" style="left:13.392px;top:12.878px;">do imposto de renda!</span></div></div><div id="spr23_1380021" style="left:90.17px;top:122.137px;"><div style="width:0px;"><span id="txt4_1380021" class="nokern" data-width="481.886719" style="left:23.387px;top:0.509px;">O cliente vai concorrer a sorteios</span></div><div style="width:0px;"><span id="txt5_1380021" class="nokern relpos" data-width="317.074219" style="left:12.327px;top:41.235px;">semanais no valor de </span><span id="txt6_1380021" class="nokern relpos" data-width="186.750000" style="left:12.322px;top:41.235px;">R$ 1.500,00!</span></div></div></div></div>');})();
